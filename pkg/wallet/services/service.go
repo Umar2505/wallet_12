87a9c171417e543726f2f79221076e792d6ddad8
Umar <rustamovumar0@gmail.com>
2022-09-07 19:48:55 +0500

    realized functions

--- pkg/wallet/services/service.go
+++ pkg/wallet/services/service.go
@@ -9,21 +9,49 @@ import (
 var ErrPaymentNotFound = errors.New("payment not found")
 
 type Service struct {
+	nextAccountID int64
 	accounts []*types.Account
-	payments []*types.Payment
 }
 
-func (s *Service) Reject(paymentID string) error  {
-	for _, v := range s.payments {
-		if v.ID==paymentID {
-			for _, a := range s.accounts {
-				if v.AccountID==a.ID {
-					a.Balance+=v.Amount
-					v.Status=types.PaymentStatusFail
-					return nil
-				}
-			}
+type testService struct {
+	*Service
+}
+
+func NewTestService() *testService {
+	return &testService{Service: &Service{}}
+}
+
+func (s *testService) Register(phone types.Phone) (*types.Account,error) {
+	for _, v := range s.accounts {
+		if v.Phone==phone {
+			return v,errors.New("phone is existed")
 		}
 	}
-	return  ErrPaymentNotFound
+	s.nextAccountID++
+	acc:= types.Account{
+		ID: s.nextAccountID,
+		Phone: phone,
+		Balance: 0,
+	}
+	s.accounts=append(s.accounts, &acc)
+	return &acc,nil
+}
+
+func (s *testService,) Deposit(accountID int64,amount types.Money) (error) {
+	if amount<=0 {
+		return errors.New("amount should be greater than 0")
+	}
+	
+	var targetAccount *types.Account
+	for _, v := range s.accounts {
+		if v.ID==accountID {
+			targetAccount=v
+		}
+	}
+	if targetAccount==nil {
+		return errors.New("account not found")
+	}
+
+	targetAccount.Balance+=amount
+	return nil
 }
