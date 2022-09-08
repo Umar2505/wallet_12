07fc6d40055e898eecbb2ca372d585bd5fa9e6de
Umar <rustamovumar0@gmail.com>
2022-09-08 22:19:11 +0500

    realized new functions

--- pkg/wallet/services/service.go
+++ pkg/wallet/services/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"strconv"
 
 	"github.com/Umar2505/wallet_12/pkg/wallet/types"
 )
@@ -9,11 +10,63 @@ import (
 var ErrPaymentNotFound = errors.New("payment not found")
 
 type Service struct {
+	nextAccountID int64
+	nextPaymentID string
 	accounts []*types.Account
 	payments []*types.Payment
 }
 
-func (s *Service) Reject(paymentID string) error  {
+type testService struct {
+	*Service
+}
+
+func NewTestService() *testService {
+	return &testService{Service: &Service{}}
+}
+
+func (s *testService) Register(phone types.Phone) (*types.Account,error) {
+	for _, v := range s.accounts {
+		if v.Phone==phone {
+			return v,errors.New("phone is existed")
+		}
+	}
+	s.nextAccountID++
+	acc:= types.Account{
+		ID: s.nextAccountID,
+		Phone: phone,
+		Balance: 0,
+	}
+	s.accounts=append(s.accounts, &acc)
+	return &acc,nil
+}
+
+func (s *testService,) Deposit(accountID int64,amount types.Money) (*types.Payment,error) {
+	var payment types.Payment
+	if amount<=0 {
+		return nil, errors.New("amount should be greater than 0")
+	}
+	
+	var targetAccount *types.Account
+	for _, v := range s.accounts {
+		if v.ID==accountID {
+			targetAccount=v
+		}
+	}
+	if targetAccount==nil {
+		return nil, errors.New("account not found")
+	}
+
+	targetAccount.Balance+=amount
+	acc,_:=strconv.Atoi(s.nextPaymentID)
+	s.nextPaymentID=strconv.Itoa(acc+1)
+	payment.ID=s.nextPaymentID
+	payment.AccountID=targetAccount.ID
+	payment.Amount=amount
+	s.payments=append(s.payments, &payment)
+	return &payment,nil
+}
+
+func (s *testService) Reject(paymentID string) error  {
 	for _, v := range s.payments {
 		if v.ID==paymentID {
 			for _, a := range s.accounts {
@@ -25,4 +78,26 @@ func (s *Service) Reject(paymentID string) error  {
 		}
 	}
 	return  ErrPaymentNotFound
+}
+
+func (s *testService) Repeat(paymentID string) (*types.Payment,error) {
+	targetP:= types.Payment{}
+	targetPayment:=&targetP
+	for _, v := range s.payments {
+		if v.ID==paymentID {
+			targetPayment.AccountID=v.AccountID
+			targetPayment.Amount=v.Amount
+			targetPayment.Category=v.Category
+			targetPayment.Status=v.Status
+			d,_:=strconv.Atoi(s.nextPaymentID)
+			s.nextPaymentID=strconv.Itoa(d+1)
+			targetPayment.ID=s.nextPaymentID
+		}
+	}
+
+	if targetPayment==nil {
+		return nil, errors.New("payment not found")
+	}
+
+	return targetPayment,nil
 }
--- pkg/wallet/services/service_test.go
+++ pkg/wallet/services/service_test.go
@@ -2,70 +2,36 @@ package service
 
 import (
 	"fmt"
-	"reflect"
+	"strconv"
 	"testing"
-
-	"github.com/Umar2505/wallet_12/pkg/wallet/types"
 )
 
-var s *Service = new(Service)
-
-var accounts = []*types.Account{
-	{
-		ID: 1,
-		Balance: 195_000,
-	},
-	{
-		ID: 2,
-		Balance: 50_000,
-	},
-	{
-		ID: 3,
-		Balance: 0,
-	},
-	{
-		ID: 4,
-		Balance: 100_000,
-	},
-}
-
-var payments= []*types.Payment{
-	{
-		ID: "10",
-		Amount: 1000,
-		AccountID: 3,
-	},
-	{
-		ID: "11",
-		Amount: 5_000,
-		AccountID: 1,
-	},
-	{
-		ID: "12",
-		Amount: 2_000,
-		AccountID: 2,
-	},
-}
-
-func TestService_Reject_positive(t *testing.T) {
-	
-	s.accounts=accounts
-	s.payments=payments
-
-	ex:= types.Account{
-		ID: 1,
-		Balance: 200_000,
-	}
-	expected := &ex
-
-	err:=s.Reject("11")
+func TestRepeat_positive(t *testing.T) {
+	s:= NewTestService()
+	acc,err:=s.Register("+123123123")
 	if err!=nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		return
+	}
+	pay,err:=s.Deposit(acc.ID,1000)
+	if err!=nil {
+		fmt.Println(err.Error())
 		return
 	}
 
-	if !reflect.DeepEqual(s.accounts[0],expected) {
-		fmt.Printf("Expected: %v, recieved: %v",expected,s.accounts[0])
+	expect:= *pay
+	add,_:=strconv.Atoi(expect.ID)
+	expect.ID=strconv.Itoa(add+1)
+
+	payment,err:=s.Repeat(pay.ID)
+	if err!=nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if expect.AccountID!=payment.AccountID && expect.Amount!=payment.Amount {
+		fmt.Println("your func() isn't work")
 		return
 	}
 }
+
