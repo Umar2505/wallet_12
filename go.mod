b845b772d7c01b975bcb7b7ddeee0a417e2ee7f5
Umar <rustamovumar0@gmail.com>
2022-09-12 10:51:01 +0500

    realized func() favorite

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Umar2505/wallet_12
 
 go 1.19
+
+require github.com/google/uuid v1.3.0
--- pkg/wallet/services/service.go
+++ pkg/wallet/services/service.go
@@ -2,9 +2,9 @@ package service
 
 import (
 	"errors"
-	"strconv"
 
 	"github.com/Umar2505/wallet_12/pkg/wallet/types"
+	"github.com/google/uuid"
 )
 
 var ErrPaymentNotFound = errors.New("payment not found")
@@ -14,6 +14,7 @@ type Service struct {
 	nextPaymentID string
 	accounts []*types.Account
 	payments []*types.Payment
+	favorites []*types.Favorite
 }
 
 type testService struct {
@@ -57,8 +58,7 @@ func (s *testService,) Deposit(accountID int64,amount types.Money) (*types.Payme
 	}
 
 	targetAccount.Balance+=amount
-	acc,_:=strconv.Atoi(s.nextPaymentID)
-	s.nextPaymentID=strconv.Itoa(acc+1)
+	s.nextPaymentID=uuid.NewString()
 	payment.ID=s.nextPaymentID
 	payment.AccountID=targetAccount.ID
 	payment.Amount=amount
@@ -89,15 +89,54 @@ func (s *testService) Repeat(paymentID string) (*types.Payment,error) {
 			targetPayment.Amount=v.Amount
 			targetPayment.Category=v.Category
 			targetPayment.Status=v.Status
-			d,_:=strconv.Atoi(s.nextPaymentID)
-			s.nextPaymentID=strconv.Itoa(d+1)
+			s.nextPaymentID=uuid.NewString()
 			targetPayment.ID=s.nextPaymentID
 		}
 	}
 
-	if targetPayment==nil {
+	if targetPayment==&targetP {
 		return nil, errors.New("payment not found")
 	}
 
 	return targetPayment,nil
+}
+
+func (s *testService) FavoritePayment(paymentID string, name string) (*types.Favorite, error) {
+	var tPayment *types.Payment
+	for _, payment := range s.payments {
+		if payment.ID==paymentID {
+			tPayment=payment
+		}
+	}
+	if tPayment==nil {
+		return nil, errors.New("couldn't find payment")
+	}
+
+	var favorite *types.Favorite
+	favorite.AccountID= tPayment.AccountID
+	favorite.Amount=tPayment.Amount
+	favorite.Category=tPayment.Category
+	favorite.ID=tPayment.ID
+	favorite.Name=name
+	s.favorites=append(s.favorites, favorite)
+	return favorite,nil
+}
+
+func (s *testService) PayFromFavorite(favoriteID string) (*types.Payment, error) {
+	var tFavorite *types.Favorite
+	for _, favorite := range s.favorites {
+		if favorite.ID==favoriteID {
+			tFavorite=favorite
+		}
+	}
+
+	if tFavorite==nil {
+		return nil,errors.New("couldn't find favorite")
+	}
+
+	payment,err:= s.Deposit(tFavorite.AccountID,tFavorite.Amount)
+	if err!=nil {
+		return nil,err
+	}
+	return payment,nil
 }
